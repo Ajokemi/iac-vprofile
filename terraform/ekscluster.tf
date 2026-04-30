c0fd152eb6e2634b37f0a5f7848018a3cd4c2394
Ajoke <wasiuajoke2011@gmail.com>
2026-04-30 04:09:27 +0100

    fix: add missing closing brace

--- terraform/ekscluster.tf
+++ terraform/ekscluster.tf
@@ -1,6 +1,8 @@
 module "eks" {
- source  = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.31"
+  ...
+}
  
   cluster_name    = local.cluster_name
   cluster_version = "1.33"
