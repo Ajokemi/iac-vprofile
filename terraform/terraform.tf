b1fcdebca68f3e8bf8a9a90ea84d2d126e2b7147
Ajoke <wasiuajoke2011@gmail.com>
2026-04-30 14:39:33 +0100

    Restore S3 backend for CI (restore)

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = "~> 1.5"
 
-  backend "s3" {
-    bucket = "" # Leave blank - workflow will fill this
-    key    = "vprofile/terraform.tfstate"
-    region = "" # Leave blank - workflow will fill this
+    backend "s3" {
+      bucket = "" # Leave blank - workflow will fill this
+      key    = "vprofile/terraform.tfstate"
+      region = "" # Leave blank - workflow will fill this
   }
 
   required_providers {
